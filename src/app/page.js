80e91d14337181c4c4a9f8007fdc682985c0629f
Paulo Santos <peedesantos40@gmail.com>
2026-07-02 19:34:19 -0300

    Publicar

--- src/app/page.js
+++ src/app/page.js
@@ -1,64 +1,34 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
-import styles from "./page.module.css";
+import { BsSun, BsMoon } from "react-icons/bs";
+import estilos from "./page.module.css";
 
 export default function Home() {
+  const [alterarPagina, setAlterarPagina] = useState(false);
+
+  function trocarTemaPagina() {
+    setAlterarPagina(!alterarPagina);
+  }
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className={alterarPagina ? estilos.dark_mode : estilos.light_mode}>
+      <header className={estilos.header}>
+        <div className={estilos.logo}>
+          <Image  src="/barbearia.png" alt="Logo" width={100} height={100} />
+          <button className={estilos.temaButton} onClick={trocarTemaPagina}>
+            {alterarPagina ? <BsSun /> : <BsMoon />}
+          </button>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </header>
+      <main className={estilos.main}>
+        <div className={estilos.container_banner}></div>
+        <div className={estilos.container_texto}>
+          <h1>Bem-vindo à Barber Shop</h1>
+          <h2>Nossa barbearia sempre oferece profissionais de qualidade e estamos prontos para lidar com suas maiores expectativas.
+          </h2>
+          <p>Nossos serviços são dedicados ao seu sucesso pessoal. Aqui temos uma equipe premiada que demonstrou o talento de mestres barbeiros em vários concursos de estilo. Deixe nosso barbeiro ser seu estilista pessoal e você nunca ficará desapontado.
+          </p>
+          <p className={estilos.assinatura}>S. Kelly</p>
         </div>
       </main>
     </div>
